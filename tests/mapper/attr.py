0790044642f10843241f99d58d852d1351d1e151
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2016-07-18 23:01:51 +0900

    Adding more deletion functionalities

--- tests/mapper/attr.py
+++ tests/mapper/attr.py
@@ -3,7 +3,7 @@
 
 """Attribute deletion test."""
 
-from unittest import TestCase, skip
+from unittest import TestCase
 
 import omm
 
@@ -33,7 +33,7 @@ class MapperDeletionTest(TestCase):
         self.cls = TestMap
         self.data_cls = DataCls
         self.data = DataCls()
-        self.mapper = self.cls(DataCls())
+        self.mapper = self.cls(self.data)
         del self.mapper.age
 
     def test_field_deletion(self):
@@ -48,7 +48,6 @@ class MapperDeletionTest(TestCase):
             del self.mapper.test
 
 
-@skip("Not Implemented Yet")
 class MapperDeletionValueFromFieldTest(TestCase):
     """Field Deletion by specified metadata from field with value Test."""
 
@@ -74,12 +73,53 @@ class MapperDeletionValueFromFieldTest(TestCase):
         self.cls = TestMap
         self.data_cls = DataCls
         self.data = DataCls()
-        self.mapper = self.cls(DataCls())
+        self.mapper = self.cls(self.data)
         del self.mapper.age
 
     def test_field_deletion(self):
         """The value should be delete."""
         self.assertNotIn("age", self.mapper.fields)
         self.assertNotIn("age", self.mapper.__dict__)
+
+    def test_attr_access(self):
+        """The attribute shouldn't exist."""
+        with self.assertRaises(AttributeError):
+            self.data.test.age
+
+
+class MapperDeletionValueFromFieldExceptionTest(TestCase):
+    """Error Test due to field vlaue deletion."""
+
+    def setUp(self):
+        """Setup."""
+        class TestMap(omm.Mapper):
+            name = omm.MapField("test.name")
+            age = omm.MapField("test.age", delete_value=True)
+            sex = omm.MapField("test.sex")
+
+        class DataCls(object):
+
+            class DataSubCls(object):
+
+                def __init__(self):
+                    self.name = "Test Example"
+                    self.sex = "test"
+
+            def __init__(self):
+                self.test = self.DataSubCls()
+
+        self.cls = TestMap
+        self.data_cls = DataCls
+        self.data = DataCls()
+        self.mapper = self.cls(self.data)
+        del self.mapper.age
+
+    def test_field_deletion(self):
+        """It raises nothing."""
+        self.assertNotIn("age", self.mapper.fields)
+        self.assertNotIn("age", self.mapper.__dict__)
+
+    def test_data_value(self):
+        """The attribute shouldn't exist."""
         with self.assertRaises(AttributeError):
             self.data.test.age
